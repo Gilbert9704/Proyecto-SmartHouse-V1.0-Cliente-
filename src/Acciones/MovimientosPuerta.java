b98d79c0f55cfb16b738d6ce59829fa2641d5bb7
Gilbert Andres Angulo Saucedo <gilbertagulo2014@gmail.com>
2016-05-31 23:34:46 -0500

    Cliente Proyecto Final de POO

---
+++ src/Acciones/MovimientosPuerta.java
@@ -0,0 +1,9 @@
+package Acciones;
+
+public interface MovimientosPuerta {
+
+	public boolean puertaAlcoba1(boolean alcoba1);
+	public boolean puertaAlcoba2(boolean alcoba2);
+	public boolean puertaBano(boolean bano);
+	public boolean puertaPrincipal(boolean puertaPrinc);
+}
---
+++ src/Acciones/Puerta.java
@@ -0,0 +1,79 @@
+package Acciones;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Puerta implements MovimientosPuerta {
+
+    private final String host = "localhost"; //<----- Modificar para otras IPs//<----- Modificar para otras IPs
+    
+    @Override
+    public boolean puertaAlcoba1(boolean alcoba1) {
+        final int num = 1;
+        try {
+            enviarComando(num, alcoba1);
+        } catch (IOException ex) {
+            Logger.getLogger(Puerta.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alcoba1;
+    }
+
+    @Override
+    public boolean puertaAlcoba2(boolean alcoba2) {
+	final int num = 2;
+        try {
+            enviarComando(num, alcoba2);
+        } catch (IOException ex) {
+            Logger.getLogger(Puerta.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	return alcoba2;
+    }
+
+    @Override
+    public boolean puertaBano(boolean bano) {
+        final int num = 3;
+        try {
+            enviarComando(num, bano);
+        } catch (IOException ex) {
+            Logger.getLogger(Puerta.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	return bano;
+    }
+
+    @Override
+    public boolean puertaPrincipal(boolean puertaPrinc) {
+	final int num = 4;
+        try {
+            enviarComando(num, puertaPrinc);
+        } catch (IOException ex) {
+            Logger.getLogger(Puerta.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	return puertaPrinc;
+    }
+    
+    public void enviarComando(int numero, boolean estPuerta) throws IOException{
+    	System.out.println("entre a enviarComando");
+        /*
+    	try{
+            Socket socket1 = new Socket(host, 8500);
+            
+            DataOutputStream datoAlServidor = new DataOutputStream(socket1.getOutputStream());
+            System.out.println("Voy a enviar los datos");
+            System.out.println(numero);
+            System.out.println(estPuerta);
+            
+            datoAlServidor.writeInt(numero);
+            datoAlServidor.flush();
+            datoAlServidor.writeBoolean(estPuerta);
+            datoAlServidor.flush();
+            
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+        */
+    }    
+}
